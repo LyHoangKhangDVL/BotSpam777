9fbaa1d17aaa7ffd7b248cf652ccbf0bf413a23a
AkiraAkaza <137365140+AkiraAkaza@users.noreply.github.com>
2024-03-04 15:27:55 +0700

    Update login.js

--- login.js
+++ login.js
@@ -39,32 +39,19 @@ function loginBot(bot) {
     }, config.delayspammer * 1000)
   })
 
-  function random() {
+function random() {
     const unicode = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    const vncode = 'AĂÂBCDĐEÊGHIKLMNOÔƠPQRSTUƯVXYÀÁẢÃẠẰẮẲẴẶẦẤẨẪẬÈÉẺẼẸỀẾỂỄỆÌÍỈĨỊÒÓỎỌÕỒỐỔỖỘỜỚỞỠỢÙÚŨỤỦỪỨỬỮỰỲÝỸỶỴaăâbcdđeêghiklmnoôơpqrstuưvxyàáảãạằắẳẵặầấẩẫậèéẻẽẹềếểẽẹìíỉĩịòóỏọõồốổỗộờớởỡợùúũụủừứửữựỳýỹỷỵ123456789'
+    const vncode = 'AĂÂBCDĐEÊGHIKLMNOÔƠPQRSTUƯVXYÀÁẢÃẠẰẮẲẴẶẦẤẨẪẬÈÉẺẼẸỀẾỂỄỆÌÍỈĨỊÒÓỎỌÕỒỐỔỖỘỜỚỞỠỢÙÚŨỤỦỪỨỬỮỰỲÝỸỶỴaăâbcdđeêghiklmnoôơpqrstuưvxyàáảãạằắẳẵặầấẩẫậèéẻẽẹềếểẽẹìíỉĩịòóỏọõồốổỗộờớởỡợùuúũụủừứửữựỳýỹỷỵ123456789';
 
-    let random1 = `${unicode.charAt(Math.floor(Math.random() * unicode.length))}`;
-    let random2 = `${unicode.charAt(Math.floor(Math.random() * unicode.length))}`;
-    let random3 = `${unicode.charAt(Math.floor(Math.random() * unicode.length))}`;
-    let random4 = `${unicode.charAt(Math.floor(Math.random() * unicode.length))}`;
-    let random5 = `${unicode.charAt(Math.floor(Math.random() * unicode.length))}`;
-    let random6 = `${unicode.charAt(Math.floor(Math.random() * unicode.length))}`;
-    let random7 = `${unicode.charAt(Math.floor(Math.random() * unicode.length))}`;
-    let random8 = `${unicode.charAt(Math.floor(Math.random() * unicode.length))}`;
-    let random9 = `${unicode.charAt(Math.floor(Math.random() * unicode.length))}`;
+    let randomChars = '';
+    let rdChars = '';
 
-    let rd1 = `${vncode.charAt(Math.floor(Math.random() * vncode.length))}`;
-    let rd2 = `${vncode.charAt(Math.floor(Math.random() * vncode.length))}`;
-    let rd3 = `${vncode.charAt(Math.floor(Math.random() * vncode.length))}`;
-    let rd4 = `${vncode.charAt(Math.floor(Math.random() * vncode.length))}`;
-    let rd5 = `${vncode.charAt(Math.floor(Math.random() * vncode.length))}`;
-    let rd6 = `${vncode.charAt(Math.floor(Math.random() * vncode.length))}`;
-    let rd7 = `${vncode.charAt(Math.floor(Math.random() * vncode.length))}`;
-    let rd8 = `${vncode.charAt(Math.floor(Math.random() * vncode.length))}`;
-    let rd9 = `${vncode.charAt(Math.floor(Math.random() * vncode.length))}`;
+    for (let i = 0; i < 20; i++) {
+        randomChars += unicode.charAt(Math.floor(Math.random() * unicode.length));
+        rdChars += vncode.charAt(Math.floor(Math.random() * vncode.length));
+    }
 
-    return `${random1}${random2}${random3}${random4}${random5}${random6}${random7}${random8}${random9}${rd1}${rd2}${rd3}${rd4}${rd5}${rd6}${rd7}${rd8}${rd9}`;
-  };
+    return randomChars + rdChars;
 }
 
 module.exports = { loginBot };
